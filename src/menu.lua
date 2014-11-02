5596dcfda86b68347bc5f20ecec2384af7addcae
Remco Meeuwissen <dearon@gmail.com>
2014-11-02 18:29:03 +0100

    Added a menu sound

--- src/menu.lua
+++ src/menu.lua
@@ -1,5 +1,9 @@
 local menu = {}
 
+function menu:load()
+    menuSound = love.audio.newSource("audio/menuSound.mp3", "static")
+end
+
 function menu:draw()
     love.graphics.clear()
     love.graphics.setBackgroundColor(255, 255, 255)
@@ -16,14 +20,17 @@ end
 function menu:mousepressed(x, y, button)
     if button == "l" then
         if x >= 440 and x <= 520 and y >= 370 and y <= 410 then
+            love.audio.play(menuSound)
             Gamestate.switch(game)
         end
 
         if x >= 660 and x <= 775 and y >= 370 and y <= 410 then
+            love.audio.play(menuSound)
             Gamestate.switch(credits)
         end
 
         if x >= 950 and x <= 1020 and y >= 370 and y <= 410 then
+            love.audio.play(menuSound)
             love.event.quit()
         end
     end
