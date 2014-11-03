77306b4956683ace24006694e40218b21c4de0da
Remco Meeuwissen <dearon@gmail.com>
2014-11-03 11:24:14 +0100

    Added some text explaining what happened to the victory condition

--- src/basement.lua
+++ src/basement.lua
@@ -9,13 +9,16 @@ local function fadeout()
 end
 
 function basement:enter()
-    Timer.tween(3, overlayColor, {0, 0, 0, 0}, "linear", function() Timer.add(1, function() fadeout() end) end)
+    Timer.tween(3, overlayColor, {0, 0, 0, 0}, "linear", function() Timer.add(5, function() fadeout() end) end)
 end
 
 function basement:draw()
     love.graphics.clear()
 
     love.graphics.setBackgroundColor(255, 255, 255)
+
+    love.graphics.setColor(0, 0, 0, 255)
+    love.graphics.printf("You open the hatch and go through into a basement of sorts.\nIt seems you've escape death... for now.", 200, 200 , 880 ,"center") 
 
     love.graphics.setColor(overlayColor)
     love.graphics.rectangle("fill", 0, 0, love.window.getWidth(), love.window.getHeight())
