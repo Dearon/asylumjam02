9c0e9619ca44a0687ecf8b197c3147d11789f70a
Remco Meeuwissen <dearon@gmail.com>
2014-11-03 10:57:29 +0100

    Fixed the credits

--- src/credits.lua
+++ src/credits.lua
@@ -2,19 +2,24 @@ local credits = {}
 
 function credits:enter()
     pos = {y = 500}
-    Timer.tween(10, pos, {y = -700}, 'linear', function() Gamestate.switch(menu) end)
+    Timer.tween(10, pos, {y = -800}, 'linear', function() Gamestate.switch(menu) end)
 end
 
 function credits:draw()
     love.graphics.clear()
     love.graphics.setBackgroundColor(255, 255, 255)
+    love.graphics.setColor(255, 255, 255)
 
     love.graphics.draw(logo, 0, pos.y + 0)
 
+    love.graphics.setColor(0, 0, 0)
+
     local text = [[This game was made for the Asylum Jam 2
-    Art and design was done by Person 1
-    Coding was done by Person 2
-    Music and SFX was done by Person 3]]
+    Art and design was done by @CScribes
+    Coding was done by @_Dearon
+    Music and SFX was done by @antisocialite
+    
+    We hope you enjoyed the game, let us know!]]
 
     love.graphics.printf(text, 250, pos.y + 480, 800, "center")
 end
