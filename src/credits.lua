c7b47bb8bb4efca9175b92a63c9f171e340abf28
Dearon <dearon@gmail.com>
2014-11-04 19:57:38 +0100

    Quick fix to the credits

--- src/credits.lua
+++ src/credits.lua
@@ -17,7 +17,7 @@ function credits:draw()
     local text = [[This game was made for the Asylum Jam 2
     Art and design was done by @CScribes
     Coding was done by @_Dearon
-    Music and SFX was done by @antisocialite
+    Music and SFX was done by @antisocialiteuk
     
     We hope you enjoyed the game, let us know!]]
 
